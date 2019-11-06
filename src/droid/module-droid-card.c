34770696f48cf35aea4dd4d3f51a023414c87473
Ratchanan Srirattanamet <ratchanan@ubports.com>
2019-11-06 23:47:35 +0700

    card: read headphone availability from input device
    Recent Android devices start to provide headphone availability via input
    device instead of h2w switch. This renders droid-extcon useless.
    
    This commit introduce droid-extevdev, a simple code that will read
    headphone availability from the input device, using libevdev as an
    abstraction layer. This means the code now depends on libevdev (but it
    can be made optional later on if needed).
    
    This should make headphone availability works on newer Android devices
    without having to resort to h2w kernel driver.

--- src/droid/module-droid-card.c
+++ src/droid/module-droid-card.c
@@ -64,6 +64,7 @@
 #include "droid-sink.h"
 #include "droid-source.h"
 #include "droid-extcon.h"
+#include "droid-extevdev.h"
 
 PA_MODULE_AUTHOR("Juho Hämäläinen");
 PA_MODULE_DESCRIPTION("Droid card");
@@ -162,6 +163,7 @@ struct userdata {
     pa_card_profile *real_profile;
 
     pa_droid_extcon *extcon;
+    pa_droid_extevdev *extevdev;
 
     pa_modargs *modargs;
     pa_card *card;
@@ -852,6 +854,11 @@ int pa__init(pa_module *m) {
     init_profile(u);
     u->extcon = pa_droid_extcon_new(m->core, u->card);
 
+    if (!u->extcon)
+        u->extevdev = pa_droid_extevdev_new(m->core, u->card);
+    else
+        u->extevdev = NULL;
+
     pa_card_put(u->card);
 
     return 0;
@@ -880,6 +887,9 @@ void pa__done(pa_module *m) {
 
         if (u->extcon)
             pa_droid_extcon_free(u->extcon);
+
+        if (u->extevdev)
+            pa_droid_extevdev_free(u->extevdev);
 
         if (u->card)
             pa_card_free(u->card);
